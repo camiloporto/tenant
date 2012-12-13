a68ef8976997502f33715f1d2f8a3fdf5530714e
unknown <UR42@NO00038342.petrobras.biz>
2012-12-13 11:37:43 -0300

    criado entidade Usuario com repositorio e servico

---
+++ src/main/java/br/com/camiloporto/tenant/model/Usuario.java
@@ -0,0 +1,22 @@
+package br.com.camiloporto.tenant.model;
+
+import javax.persistence.Column;
+import javax.validation.constraints.NotNull;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.jpa.entity.RooJpaEntity;
+import org.springframework.roo.addon.serializable.RooSerializable;
+import org.springframework.roo.addon.tostring.RooToString;
+
+@RooJavaBean
+@RooToString
+@RooJpaEntity
+@RooSerializable
+public class Usuario {
+
+    @NotNull
+    @Column(unique = true)
+    private String email;
+
+    @NotNull
+    private String password;
+}
