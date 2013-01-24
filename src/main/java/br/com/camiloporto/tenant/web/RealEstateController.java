180fd4acaf1e895c756bda08363e19cf51698925
Camilo Porto <camiloporto@badenbaden.(none)>
2013-01-23 21:42:52 -0300

    adicionada tela de detalhes do imovel

--- src/main/java/br/com/camiloporto/tenant/web/RealEstateController.java
+++ src/main/java/br/com/camiloporto/tenant/web/RealEstateController.java
@@ -3,6 +3,7 @@ package br.com.camiloporto.tenant.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RequestMapping("/realestates")
 @Controller
@@ -11,6 +12,11 @@ public class RealEstateController {
 	@RequestMapping(method = RequestMethod.GET)
 	public String home() {
 		return "realestate/index";
+	}
+	
+	@RequestMapping(params={"id"}, method = RequestMethod.GET)
+	public String detail(@RequestParam("id") Long idRealEstate) {
+		return "realestate/detail";
 	}
 
 }
