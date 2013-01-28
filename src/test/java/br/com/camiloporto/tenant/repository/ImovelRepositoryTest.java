d857479e0b6ca811333712d6ffe2ea8f74913c4a
unknown <UR42@NO00038342.petrobras.biz>
2013-01-28 16:47:02 -0300

    implementado teste para ordenacao dos imoveis com base na data de atualizacao

--- src/test/java/br/com/camiloporto/tenant/repository/ImovelRepositoryTest.java
+++ src/test/java/br/com/camiloporto/tenant/repository/ImovelRepositoryTest.java
@@ -3,9 +3,13 @@ package br.com.camiloporto.tenant.repository;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.domain.Sort.Order;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import br.com.camiloporto.tenant.model.Imovel;
@@ -15,6 +19,11 @@ public class ImovelRepositoryTest extends AbstractTestNGSpringContextTests {
 	
 	@Autowired
 	private ImovelRepository repository;
+	
+	@BeforeMethod
+	public void cleanAllData() {
+		repository.deleteAll();
+	}
 	
 	@Test
 	public void deveListarTodosOsImoveis() {
@@ -35,31 +44,36 @@ public class ImovelRepositoryTest extends AbstractTestNGSpringContextTests {
 	}
 	
 	@Test
-	public void deveListarTodosOsImoveisEmOrdemDecrescenteDeDataAtualizacao() {
+	public void deveListarTodosOsImoveisEmOrdemDecrescenteDeDataAtualizacao() throws InterruptedException {
+		final String ruaImovel1 = "Tereza Campos";
+		final String ruaImovel2 = "Av Integracao";
 		Imovel i1 = new ImovelBuilder()
 			.doTipo("Apartamento")
 			.noEstado("RN")
 			.naCidade("Natal")
 			.noBairro("Lagoa Nova")
-			.naRua("Tereza Campos")
+			.naRua(ruaImovel1)
 			.create();
+		
+		repository.save(i1);
+		Thread.sleep(1000);
 		
 		Imovel i2 = new ImovelBuilder()
 			.doTipo("Apartamento")
 			.noEstado("RN")
 			.naCidade("Natal")
 			.noBairro("Candelaria")
-			.naRua("Av integracao")
+			.naRua(ruaImovel2)
 			.create();
 	
-		repository.save(i1);
+		
 		repository.save(i2);
 		
-		List<Imovel> all = repository.findAll();
+		List<Imovel> all = repository.findAll(new Sort(new Order(Direction.DESC, "ultimaAtualizacao")));
 		
 		Assert.assertEquals(all.size(), 2, "numero de imoveis diferente do esperado");
-		Assert.assertEquals(all.get(0).getRua(), "Av Integracao", "ordem dos imoveis diferente do esperado");
-		Assert.assertEquals(all.get(1).getRua(), "Tereza Campos", "ordem dos imoveis diferente do esperado");
+		Assert.assertEquals(all.get(0).getRua(), ruaImovel2, "ordem dos imoveis diferente do esperado");
+		Assert.assertEquals(all.get(1).getRua(), ruaImovel1, "ordem dos imoveis diferente do esperado");
 		
 	}
 }
