71b669248b3a816b8337ed0836ed59eeb789ff23
Camilo Porto <camiloporto@badenbaden.(none)>
2013-01-29 14:13:28 -0300

    configurado teste Web MVC e testado listagem de imoveis

--- src/main/java/br/com/camiloporto/tenant/web/RealEstateController.java
+++ src/main/java/br/com/camiloporto/tenant/web/RealEstateController.java
@@ -1,16 +1,30 @@
 package br.com.camiloporto.tenant.web;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.camiloporto.tenant.model.Imovel;
+import br.com.camiloporto.tenant.service.ImovelService;
 
 @RequestMapping("/realestates")
 @Controller
 public class RealEstateController {
 	
+	@Autowired
+	private ImovelService imovelService;
+	
 	@RequestMapping(method = RequestMethod.GET)
-	public String home() {
-		return "realestate/index";
+	public ModelAndView home() {
+		ModelAndView mav = new ModelAndView("realestate/index");
+		List<Imovel> imoveis = imovelService.findAllSortedByUltimaAtualizacao();
+		mav.addObject("imoveis", imoveis);
+		
+		return mav;
 	}
 
 }
--- src/test/java/br/com/camiloporto/tenant/controller/RealEstateControllerTest.java
+++ src/test/java/br/com/camiloporto/tenant/controller/RealEstateControllerTest.java
@@ -1,30 +1,69 @@
 package br.com.camiloporto.tenant.controller;
 
-import static org.springframework.test.web.server.setup.MockMvcBuilders.standaloneSetup;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.server.MockMvc;
+import org.springframework.test.web.server.MvcResult;
 import org.springframework.test.web.server.request.MockMvcRequestBuilders;
 import org.springframework.test.web.server.result.MockMvcResultMatchers;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.test.web.server.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.ModelAndView;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import br.com.camiloporto.tenant.web.RealEstateController;
+import br.com.camiloporto.tenant.model.Imovel;
+import br.com.camiloporto.tenant.repository.ImovelBuilder;
+import br.com.camiloporto.tenant.repository.ImovelRepository;
 
-@ContextConfiguration(locations = { "/META-INF/spring/applicationContext.xml","/META-INF/spring/applicationContext-jpa.xml" })
-public class RealEstateControllerTest {
+@ContextConfiguration(locations = { 
+		"/META-INF/spring/applicationContext.xml",
+		"/META-INF/spring/applicationContext-jpa.xml",
+		"/META-INF/spring/webmvc-config.xml"})
+@WebAppConfiguration
+public class RealEstateControllerTest extends AbstractTestNGSpringContextTests {
+	
+	@Autowired
+	private ImovelRepository imovelRepository;
+	
+	@Autowired 
+	private WebApplicationContext wac;
+	
+	private MockMvc mockMvc;
+	
+	@BeforeClass
+	public void setUpMockMvc() {
+		mockMvc = MockMvcBuilders.webApplicationContextSetup(wac).build();
+		Imovel i = new ImovelBuilder()
+			.doTipo("Apartamento")
+			.naCidade("Natal")
+			.noEstado("RN")
+			.noBairro("Lagoa Nova")
+			.naRua("Tereza Campos")
+			.create();
+			imovelRepository.save(i);
+	}
 	
 	@Test
 	public void deveRetornarViewPrincipalDeImoveis() throws Exception {
-		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-		viewResolver.setPrefix("/WEB-INF/views/");
-		viewResolver.setSuffix(".jsp");
-		
-		standaloneSetup(new RealEstateController())
-			.setViewResolvers(viewResolver)
-			.build()
-			.perform(MockMvcRequestBuilders.get("/realestates"))
+		MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/realestates"))
 				.andExpect(MockMvcResultMatchers.status().isOk())
 				.andExpect(MockMvcResultMatchers.model().attributeExists("imoveis"))
-				.andExpect(MockMvcResultMatchers.forwardedUrl("/WEB-INF/views/realestate/index.jsp"));
+				.andExpect(MockMvcResultMatchers.forwardedUrl("/WEB-INF/views/realestate/index.jsp"))
+				.andReturn();
+		ModelAndView mav = result.getModelAndView();
+		List<Imovel> imoveis = (List<Imovel>) mav.getModelMap().get("imoveis");
+		
+		final int expectedSize = 1;
+		final String expectedRua = "Tereza Campos";
+		Assert.assertEquals(imoveis.size(), expectedSize, "numero de imoveis diferente do esperado");
+		
+		Imovel i = imoveis.get(0);
+		Assert.assertEquals(i.getRua(), expectedRua, "nome da rua do imovel diferente do esperado");
 	}
 }
