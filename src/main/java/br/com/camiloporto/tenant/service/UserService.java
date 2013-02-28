e8f8ad2d06c463d197ba0a0005dfa0e9fcd7e5b7
unknown <UR42@NO00038342.petrobras.biz>
2013-02-28 11:30:39 -0300

    retirado codigo e configuracao de banco de dados.. usando somente elastic search

--- src/main/java/br/com/camiloporto/tenant/service/UserService.java
+++ src/main/java/br/com/camiloporto/tenant/service/UserService.java
@@ -2,6 +2,6 @@ package br.com.camiloporto.tenant.service;
 
 import org.springframework.roo.addon.layers.service.RooService;
 
-@RooService(domainTypes = { br.com.camiloporto.tenant.model.Usuario.class })
+//@RooService(domainTypes = { br.com.camiloporto.tenant.model.Usuario.class })
 public interface UserService {
 }
